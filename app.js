7042d6d1c406b6563f4eb1107bfdb7cbc9f43b7d
Rafael m <rafael.24morales@gmail.com>
2018-08-08 20:41:22 -0500

    Este es un juego de turnos aún sin la implementación de javascript

---
+++ app.js
@@ -0,0 +1,12 @@
+/*
+Reglas de juego!
+
+El juego tiene dos jugadores, los cuales participan por turno
+En cada turno, un jugador lanza el dado las veces que quiera y
+el resultado de dichos dados es sumado al puntaje de RONDA de dicho jugador
+Pero si el jugador obtiene un 1, todo el puntaje de la ronda
+se pierde y continúa el siguiente jugador.
+Cada jugador en su ronda puede elegir 'Hold', lo que significa que el puntaje de RONDA
+se es sumado a su puntuación global, depues de esto, es el turno del siguiente jugador.
+El que logre completar 100 puntos gana! :D
+*/
