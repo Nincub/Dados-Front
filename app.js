8cf97413679e9eb1eb39e91c4c8099e290220cf9
Nincub <nincub111@gmail.com>
2018-08-13 02:18:38 -0500

    Se termina el juego

--- app.js
+++ app.js
@@ -10,3 +10,109 @@ Cada jugador en su ronda puede elegir 'Hold', lo que significa que el puntaje de
 se es sumado a su puntuación global, depues de esto, es el turno del siguiente jugador.
 El que logre completar 100 puntos gana! :D
 */
+
+var turno;
+
+document.getElementById("newgame").addEventListener("click", function () {
+   setScore0(0);
+   setScore1(0);
+   setCurrent0(0);
+   setCurrent1(0);
+   changeActive(1);
+});
+document.getElementsByClassName("btn-roll")[0].addEventListener("click", function () {
+    let ran = random();
+    document.getElementsByClassName("dice")[0].src = "dice-" + ran + ".png";
+    document.getElementsByClassName("dice")[0].alt = "dice-" + ran;
+    if (ran !== 1) {
+        if (turno === 1) {
+            ran += getCurrent0();
+            setCurrent0(ran);
+        } else {
+            ran += getCurrent1();
+            setCurrent1(ran);
+        }
+    } else {
+        if (turno === 1) {
+            setCurrent0(0);
+            changeActive(2);
+        } else {
+            setCurrent1(0);
+            changeActive(1);
+        }
+        
+    }
+});
+
+document.getElementsByClassName("btn-hold")[0].addEventListener("click", function () {
+    if (turno === 1) {
+        setScore0(getScore0() + getCurrent0());
+        setCurrent0(0);
+        changeActive(2);
+    } else {
+        setScore1(getScore1() + getCurrent1());
+        setCurrent1(0);
+        changeActive(1);
+    }
+});
+
+function changeActive (a) {
+    document.getElementsByClassName("player-0-panel")[0].className = document.getElementsByClassName("player-0-panel")[0].className.replace("active", "");
+    document.getElementsByClassName("player-1-panel")[0].className = document.getElementsByClassName("player-1-panel")[0].className.replace("active", "");
+    switch (a) {
+        case 1:
+            document.getElementsByClassName("player-0-panel")[0].className += " active";
+            turno = a;
+            break;
+        case 2:
+            document.getElementsByClassName("player-1-panel")[0].className += " active";
+            turno = a;
+            break;
+        default : 
+            console.log("se paso un valor no valido");
+            break;
+    }
+}
+
+function setScore0 (a) {
+    document.getElementById("score-0").innerHTML = a;
+}
+
+function getScore0 () {
+    return parseInt(document.getElementById("score-0").innerHTML);
+}
+
+function setScore1 (a) {
+    document.getElementById("score-1").innerHTML = a;
+}
+
+function getScore1 () {
+    return parseInt(document.getElementById("score-1").innerHTML);
+}
+
+function setCurrent0 (a) {
+    document.getElementById("current-0").innerHTML = a;
+}
+
+function getCurrent0 () {
+    return parseInt(document.getElementById("current-0").innerHTML);
+}
+
+function setCurrent1 (a) {
+    document.getElementById("current-1").innerHTML = a;
+}
+
+function getCurrent1 () {
+    return parseInt(document.getElementById("current-1").innerHTML);
+}
+
+function random () {
+    let date = new Date();   
+    let mili =  parseFloat(date.getMilliseconds());
+    mili = 1.0 / mili;
+    mili *= 10000;
+    mili = parseInt(mili);
+    mili %= 6;
+    return mili + 1;
+}
+
